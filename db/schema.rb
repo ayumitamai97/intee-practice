8f3f2f2f688e3989b6de880bfdf527098d4ed46f
Ayumi Tamai <ayumitamai97@gmail.com>
2017-11-09 06:11:20 +0000

    adding column age

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171101161835) do
+ActiveRecord::Schema.define(version: 20171109060429) do
 
   create_table "messages", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20171101161835) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "age"
   end
 
 end
